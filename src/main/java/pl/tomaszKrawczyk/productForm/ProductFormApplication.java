076136f6763d83bf8e6b3ac731e5a49edde34fb9
Tomasz Krawczyk <tom_krawczyk@op.pl>
2018-04-14 16:44:03 +0200

    Second commit

---
+++ src/main/java/pl/tomaszKrawczyk/productForm/ProductFormApplication.java
@@ -0,0 +1,12 @@
+package pl.tomaszKrawczyk.productForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFormApplication.class, args);
+	}
+}
